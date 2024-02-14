d5896a2d94f73a61c18cad4af12819281d71c2e3
beckb <111229707+TheBusinessYuval@users.noreply.github.com>
2024-02-14 12:54:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/furnishare/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.furnishare;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView testPic;
+    public Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testPic = findViewById(R.id.testPic);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        testPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changePic();
+            }
+        });
+    }
+    private void changePic()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction((Intent.ACTION_GET_CONTENT));
+        ActivityResultLauncher.launch(intent);
+    }
+
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<Intent> ActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        imageUri = data.getData();
+                        testPic.setImageURI(imageUri);
+                        uploadPicture();
+                    }
+                }
+            });
+
+
+
+    private void uploadPicture() {
+        Uri file = imageUri;
+        StorageReference userImgRef = storageReference.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = userImgRef.putFile(file);
+
+// Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+              TextView tester =  findViewById(R.id.tester);
+              tester.setText("working");
+
+            }
+        });
+    }
+}
