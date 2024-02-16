06719b770b5c7594ce498eb52cc44835f2eae28f
beckb <111229707+TheBusinessYuval@users.noreply.github.com>
2024-02-16 12:44:13 +0200

    made the database real time

--- app/src/main/java/com/example/furnishare/MainActivity.java
+++ app/src/main/java/com/example/furnishare/MainActivity.java
@@ -11,34 +11,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView testPic;
     public Uri imageUri;
-    private FirebaseStorage storage;
-    private StorageReference storageReference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testPic = findViewById(R.id.testPic);
-
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
 
         testPic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void changePic()
-    {
+
+    private void changePic() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction((Intent.ACTION_GET_CONTENT));
@@ -73,26 +79,45 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-
     private void uploadPicture() {
         Uri file = imageUri;
-        final String randomkey  = UUID.randomUUID().toString();
-        StorageReference userImgRef = storageReference.child("images/"+randomkey);
-        UploadTask uploadTask = userImgRef.putFile(file);
+        final String randomkey = UUID.randomUUID().toString();
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
 
-// Register observers to listen for when the download is done or if it fails
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful uploads
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-              TextView tester =  findViewById(R.id.tester);
-              tester.setText("working");
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        List<byte[]> list = Arrays.asList(baos.toByteArray());
+        TextView tester = findViewById(R.id.tester);
+        tester.setText("staring");
+        FirebaseDatabase.getInstance("https://furnishare-551b7-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("test").push().setValue(list.toString())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                TextView tester = findViewById(R.id.tester);
+                tester.setText("worked");
             }
-        });
+        })
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                TextView tester = findViewById(R.id.tester);
+                tester.setText("didn't worked");
+            }
+        })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        TextView tester = findViewById(R.id.tester);
+                        tester.setText("canceled");
+                    }
+                });
+
+        }
     }
-}
+
